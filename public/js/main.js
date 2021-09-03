abf1af99d6d6b0c732af6ee2dbf3355d6b9c88b7
Ster <sterz.dev@gmail.com>
2021-09-03 16:46:32 -0400

    Added Modals, Account Dropdown, and Search Bar

---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+var exampleModal = $('#contactModal')
+var contactForm = $('#contact-modal-form')
+
+exampleModal.on('show.bs.modal', function (event) {
+  let button = event.relatedTarget
+  // TODO: Load current contact values into modal
+  $('#modal-contact-name').val('');
+  $('#modal-contact-phone').val('');
+  $('#modal-contact-email').val('');
+})
+
+$('#modal-contact-phone').on('focusout', function() {
+  if(validatePhoneNumber($(this).val())){}
+    $(this).val(formatPhoneNumber($(this).val()))
+})
+
+function cleanPhoneNumber(text) {
+  return text.replace(/\D/g, '');
+}
+
+function formatPhoneNumber(text) {
+  let phoneNumber = libphonenumber.parsePhoneNumber(text, "US")
+  if(text.startsWith("+"))
+    return phoneNumber.formatInternational()
+  return phoneNumber.formatNational();
+}
+
+function validatePhoneNumber(text) {
+  return libphonenumber.isPossiblePhoneNumber(text, "US")
+}
+
+jQuery.validator.addMethod('validPhone', function(val, elem) {
+  return val.length == 0 || validatePhoneNumber(cleanPhoneNumber(val))
+}, "Please enter a valid phone number.")
+
+$(document).on('ready', function() {
+  contactForm.validate({
+    rules: {
+      name : {
+        required: true
+      },
+      phone: {
+        required: false,
+        validPhone: true,
+      },
+      email: {
+        required: false,
+        email: true
+      }
+    }
+  });
+});
