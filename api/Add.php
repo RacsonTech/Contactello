02db46a2346167f46fbcc4dc13df1092974fd977
doshir20 <55893938+doshir20@users.noreply.github.com>
2021-09-10 15:00:32 -0400

    Add files via upload

---
+++ api/Add.php
@@ -0,0 +1,39 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$conn = new mysqli("localhost", "test_api", "api123", "ContactDB");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	}
+    else if (strlen($inData["FirstName"]) > 50 || strlen($inData["LastName"]) > 50 || strlen($inData["FirstName"]) < 1 || strlen($inData["LastName"]) < 1){
+        echo "Invalid input： FirstName, LastName should be within 1~50 characters";
+    }
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserID,FirstName,LastName,PhoneNumber,Email) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("sssss",  $inData["UserID"], $inData["FirstName"],  $inData["LastName"], $inData["PhoneNumber"], $inData["Email"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
